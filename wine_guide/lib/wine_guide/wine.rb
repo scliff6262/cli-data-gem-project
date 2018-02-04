fa2e597773f56b17f66d96d06a53bb89810ea274
Sean Clifford <sclifford6262@gmail.com>
2018-02-04 20:08:00 +0000

    created Wine.create_from_array

--- wine_guide/lib/wine_guide/wine.rb
+++ wine_guide/lib/wine_guide/wine.rb
@@ -1,13 +1,19 @@
 class Wine
   attr_accessor :name, :rank, :vintage, :price, :about, :score
 
-  @@all =[] 
+  @@all =[]
 
   def initialize#(wine_hash)
     wine_hash.each do |key, value|
-      wine_obj.send("#{key}=", value)
+      self.send("#{key}=", value)
     end
     @@all << wine_obj
+  end
+
+  def self.create_from_array(wines_array)
+    wines_array.each do |each_wine_hash|
+      new_wine_obj = self.new(each_wine_hash)
+    end
   end
 
   def self.all
