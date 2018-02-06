a36ab5765b44b3b69d732d97db762cca62c001fd
Sean Clifford <sclifford6262@gmail.com>
2018-02-06 20:52:26 +0000

    changed notes/format to match James Suckling instead of Wine Spectator. Also subbed .each loop to set hashes

--- wine_guide/lib/wine_guide/scraper.rb
+++ wine_guide/lib/wine_guide/scraper.rb
@@ -4,4 +4,28 @@ require 'pry'
 
 class Scraper
 
+  def self.scrape_top_100(url)
+    wine_hashes = [] 
+    html = Nokogiri::HTML(open(url))
+    binding.pry
+
+  end
+
+end
+
+Scraper.scrape_top_100("https://www.jamessuckling.com/wine-tasting-reports/top-100-wines-2017")
+
+# /\s\d+/ highlight Vintage
+# /\D/ highlight text only
+# Use index +1 for Suckling rank
+
+wine_cards = html.css(".tasting_note").each do  |each_wine|
+
+wine_name = .css("h3").text.match(/\D+/)[0]
+wine_vintage = css(".vintage").text
+wine_about = .css(".description").text
+wine_country = .css(".country").text
+wine_region = .css(".region").text
+wine_score = .css(".count").text
+
 end
